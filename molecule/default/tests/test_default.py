62c3bf5c8b3e04c7a37616637834215c37e47dca
Leonid Albrekht <lalbrekht@gmail.com>
2019-01-23 17:33:12 +0500

    Make this repo idempotent

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+# Test is /etc/hosts file is exist and have root:root attr
 def test_hosts_file(host):
     f = host.file('/etc/hosts')
 
@@ -13,8 +14,8 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
-
-def test_nginx_running_and_enabled(host):
-    nginx = host.service("nginx")
-    assert nginx.is_running
-    assert nginx.is_enabled
+# Nginx example test
+# def test_nginx_running_and_enabled(host):
+#    nginx = host.service("nginx")
+#    assert nginx.is_running
+#    assert nginx.is_enabled
